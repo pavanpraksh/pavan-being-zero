2f7b77430ea8b32393978b79a637cfe853203ea6
pavanpraksh <pavanprakash2951@gmail.com>
2019-12-19 16:13:22 +0530

    eve

--- index.js
+++ index.js
@@ -3,5 +3,8 @@ const app = express()
 const port = process.env.PORT||3000
 app.use(express.static('public'))
 app.get('/', (req, res) => res.sendFile(__dirname+'/public/html/index.html'))
-
+var obj={}
+obj.name="being zero"
+obj.college="CMRCET"
+obj.regno="17H51A05H4"
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
